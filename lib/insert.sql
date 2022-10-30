01f1792e2b118269285889aa96a563db861bc9a8
chrischep <christine.chepleting@student.moringaschool.com>
2022-10-30 08:33:30 +0300

    lab completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 56, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 7, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 78, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 23, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 52, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 70, "M", "black", "aggressive", 0);
